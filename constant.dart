272df52de1da6682a60e033665867f344054af89
N_Buddhini <39161490+nadeeshabuddhini@users.noreply.github.com>
2019-10-21 12:52:02 +0530

    Add files via upload

---
+++ constant.dart
@@ -0,0 +1,15 @@
+void main(){
+  final cityName='mumbai';
+  final String contryName='India';
+  const PI=3.14;
+  const double gravity=9.7;
+  print(cityName);
+  print(contryName);
+  print(PI);
+  print(gravity);
+
+}
+class Circle{
+  final color='Red';
+  static const PI=3.14;
+}
